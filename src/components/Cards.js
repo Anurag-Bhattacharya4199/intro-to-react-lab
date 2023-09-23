8126685f66fb40ee015bc4275ec92b0c5901341b
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2023-09-23 17:19:15 -0400

    ReactLab Completed

---
+++ src/components/Cards.js
@@ -0,0 +1,27 @@
+import "../styles/Cards.css";
+
+function Cards() {
+  return (
+    <main>
+      <section class="card-list">
+        <div class="card">
+          <div class="card__content">Content</div>
+          <h4 class="card__title">Title</h4>
+        </div>
+      </section>
+      <section class="activity-list">
+        <div>
+          <h3>Activity Feed</h3>
+          <div class="activity-list__item">Activity</div>
+          <div class="activity-list__item">Activity</div>
+          <div class="activity-list__item">Activity</div>
+          <div class="activity-list__item">Activity</div>
+          <div class="activity-list__item">Activity</div>
+          <div class="activity-list__item">Activity</div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Cards;
